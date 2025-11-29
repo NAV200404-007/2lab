4eb2e5c5846ec5bc73b7615ea0cf808f70eb96be
NAV200404-007 <KGNNAVEEN04.24@ichat.sp.edu.sg>
2025-11-29 11:55:06 +0800

    added bmi.py,coded bmi cal

---
+++ bmi.py
@@ -0,0 +1,15 @@
+def calculate_bmi(height,weight):
+    print("Height=" + str(height))
+    print("Weight=" + str(weight))
+
+    bmi = weight/(height*height)
+    print(bmi)
+
+    if(bmi < 18.5):
+        print("Under weight")
+    elif(bmi < 25):
+        print("Normal Weight")
+    else:
+        print("Over Weight")
+
+calculate_bmi(weight=79,height=1.80)
