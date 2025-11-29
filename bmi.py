ca106dad4fa827a7c6258a5687e08fa6ad136272
NAV200404-007 <KGNNAVEEN04.24@ichat.sp.edu.sg>
2025-11-29 22:33:54 +0800

    added return val

--- bmi.py
+++ bmi.py
@@ -7,10 +7,13 @@ def calculate_bmi(height,weight):
 
     if(bmi < 18.5):
         print("Under weight")
+        return -1
     elif(bmi < 25):
         print("Normal Weight")
+        return 0
     else:
         print("Over Weight")
+        return 1
 
 calculate_bmi(weight=79,height=1.80)
 
