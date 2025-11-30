8b1624a8696d0251d1f25cda1501e883d849e799
NAV200404-007 <KGNNAVEEN04.24@ichat.sp.edu.sg>
2025-11-30 11:37:32 +0800

    added test for Lab2

--- bmi.py
+++ bmi.py
@@ -8,7 +8,7 @@ def calculate_bmi(height,weight):
     if(bmi < 18.5):
         print("Under weight")
         return -1
-    elif(bmi < 25):
+    elif(bmi <= 25):
         print("Normal Weight")
         return 0
     else:
